0ed75b45774352767fae817bc976058e624171c7
Ahmed-Magdy28 <ahmedmagdy2849@gmail.com>
2026-02-13 20:29:31 +0200

    TestRoadMapPhase1

--- src/pages/Public/RoadMapPage.tsx
+++ src/pages/Public/RoadMapPage.tsx
@@ -1,22 +1,22 @@
 import RoadMapPhase1 from '@/components/common/RoadMap/RoadMapPhase1';
-import RoadMapPhase2 from '@/components/common/RoadMap/RoadMapPhase2';
-import RoadMapPhase25 from '@/components/common/RoadMap/RoadMapPhase25';
-import RoadMapPhase3 from '@/components/common/RoadMap/RoadMapPhase3';
-import RoadMapPhase4 from '@/components/common/RoadMap/RoadMapPhase4';
-import RoadMapPhase45 from '@/components/common/RoadMap/RoadMapPhase45';
-import RoadMapPhase5 from '@/components/common/RoadMap/RoadMapPhase5';
+// import RoadMapPhase2 from '@/components/common/RoadMap/RoadMapPhase2';
+// import RoadMapPhase25 from '@/components/common/RoadMap/RoadMapPhase25';
+// import RoadMapPhase3 from '@/components/common/RoadMap/RoadMapPhase3';
+// import RoadMapPhase4 from '@/components/common/RoadMap/RoadMapPhase4';
+// import RoadMapPhase45 from '@/components/common/RoadMap/RoadMapPhase45';
+// import RoadMapPhase5 from '@/components/common/RoadMap/RoadMapPhase5';
 
 export default function RoadMapPage() {
    return (
       <>
          <RoadMapPhase1 />
 
-         <RoadMapPhase2 />
+         {/* <RoadMapPhase2 />
          <RoadMapPhase25 />
          <RoadMapPhase3 />
          <RoadMapPhase4 />
          <RoadMapPhase45 />
-         <RoadMapPhase5 />
+         <RoadMapPhase5 /> */}
       </>
    );
 }
